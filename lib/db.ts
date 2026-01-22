0dc4ca1cd0e99f27be588858afdcb4bf7bc236db
ashreekar <ashreekar767@gmail.com>
2026-01-22 12:57:04 +0530

    add mongo connection

---
+++ lib/db.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error("Define monogo uri in env varibales");
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = {
+        conn: null,
+        promise: null
+    }
+}
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        }
+
+        mongoose.connect(`${MONGODB_URI}/vivi`, opts)
+            .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+}
